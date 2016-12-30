32f494b9a00991505197263903c4a426630ebbc7
logosK <logicalkink@gmail.com>
2016-12-30 05:58:28 -0500

    Added herm fairy & consolidated fairy genders; Airi choices no longer spam

--- NightgamesMod/nightgames/characters/body/Body.java
+++ NightgamesMod/nightgames/characters/body/Body.java
@@ -816,7 +816,7 @@ public class Body implements Cloneable {
             double hotness = (getHotness(opponent) - 1) / 2 + 1;
             double perception = (1.0 + (opponent.get(Attribute.Perception) - 5) / 10.0);
             if (Global.isDebugOn(DebugFlags.DEBUG_DAMAGE)) {
-                System.out.println(String.format("Seduction Bonus: %.1f, hotness: %.1f, perception: %.1f", seductionBonus, hotness, perception));
+                //System.out.println(String.format("Seduction Bonus: %.1f, hotness: %.1f, perception: %.1f", seductionBonus, hotness, perception));
             }
             double perceptionBonus = (hotness + seductionBonus) * perception;
 
